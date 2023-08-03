d40c1f84257ca5777539af8cfa58a3495b979cac
Marchelo77 <138241738+Marchelo77@users.noreply.github.com>
2023-08-04 01:06:11 +0300

    Update views.py

--- retro_cars/retro_cars/contact/views.py
+++ retro_cars/retro_cars/contact/views.py
@@ -1,5 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def contact_page(request):
-    return render(request, 'contacts/contact.html')
+    user = request.user
+    user_full_name = f'{user.first_name} {user.last_name}'
+    user_email = user.email
+
+    if user.first_name is None and user.last_name is None:
+        user_full_name = user.username
+    elif user.first_name is None:
+        user_full_name = user.last_name
+    elif user.last_name is None:
+        user_full_name = user.first_name
+
+    context = {
+        'user_full_name': user_full_name,
+        'user_email': user_email,
+    }
+    return render(request, 'contacts/contact.html', context)
