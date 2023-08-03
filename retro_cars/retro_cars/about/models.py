d9e7d42eed94e0a537a4cd41c4cfffcb415bafb3
Marchelo77 <138241738+Marchelo77@users.noreply.github.com>
2023-08-04 01:03:25 +0300

    Update models.py

--- retro_cars/retro_cars/about/models.py
+++ retro_cars/retro_cars/about/models.py
@@ -7,12 +7,22 @@ from retro_cars.about.validators import valid_date_validator
 
 
 class Event(models.Model):
-    event_name = models.CharField(max_length=30)
+    EVENT_MAX_LENGTH = 30
+    DESCRIPTION_MAX_LENGTH = 500
+    EVENT_LOCATION_MAX_LENGTH = 100
 
-    description = models.TextField(max_length=500)
+    event_name = models.CharField(
+        max_length=EVENT_MAX_LENGTH
+    )
+
+    description = models.TextField(
+        max_length=DESCRIPTION_MAX_LENGTH
+    )
 
     event_date = models.DateField()
 
-    event_location = models.CharField(max_length=100)
+    event_location = models.CharField(
+        max_length=EVENT_LOCATION_MAX_LENGTH
+    )
 
     image_url = models.URLField()
