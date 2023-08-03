153c3f546d7ca6781c2280964525f0541255b054
Marchelo77 <138241738+Marchelo77@users.noreply.github.com>
2023-08-04 01:28:51 +0300

    Added admin groups in auth_app

--- retro_cars/retro_cars/auth_app/forms.py
+++ retro_cars/retro_cars/auth_app/forms.py
@@ -1,20 +1,26 @@
 from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UsernameField
+from django.contrib.auth.models import Group
 
 UserModel = get_user_model()
 
 
 class LoginUserForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'placeholder': 'Type your username'}))
+    username = UsernameField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Type your username'
+            }
+        )
+    )
     password = forms.CharField(
         strip=False,
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Type your password'
         })
     )
-
-    # class Meta(auth_forms.AuthenticationForm.Meta):
 
 
 class RegisterUserForm(UserCreationForm):
@@ -47,3 +53,36 @@ class AppUserEditForm(forms.ModelForm):
             'gender': 'Gender',
             'profile_picture': 'Profile Picture',
         }
+        error_messages = {
+            'profile_picture': {
+                'invalid': 'Please enter a valid URL for the profile picture.',
+            },
+        }
+
+
+class GroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        exclude = []
+
+    users = forms.ModelMultipleChoiceField(
+         queryset=UserModel.objects.all(),
+         required=False,
+
+         widget=FilteredSelectMultiple('users', False)
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+
+        if self.instance.pk:
+            self.fields['users'].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        self.instance.user_set.set(self.cleaned_data['users'])
+
+    def save(self, *args, **kwargs):
+        instance = super(GroupAdminForm, self).save()
+        self.save_m2m()
+        return instance
+
