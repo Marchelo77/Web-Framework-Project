b44bd2f8a8679a177cefc030715b009750f12e29
Marchelo77 <138241738+Marchelo77@users.noreply.github.com>
2023-08-05 01:01:22 +0300

    Update contact urls.py

--- retro_cars/retro_cars/contact/urls.py
+++ retro_cars/retro_cars/contact/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from retro_cars.contact.views import contact_page
+from retro_cars.contact.views import contact_page, contacted_page
 
 urlpatterns = [
-    path('', contact_page, name="contact page")
+    path('', contact_page, name="contact page"),
+    path('contacted/', contacted_page, name="contacted page")
 ]
