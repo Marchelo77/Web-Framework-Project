effdbec0489009ca5fe2552da9ca18103ae1f3f0
Marchelo77 <138241738+Marchelo77@users.noreply.github.com>
2023-08-05 00:59:44 +0300

    Update auth_app apps.py

--- retro_cars/retro_cars/auth_app/apps.py
+++ retro_cars/retro_cars/auth_app/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'retro_cars.auth_app'
+    
+    def ready(self):
+        import retro_cars.auth_app.signals
+        result = super().ready()
+        return result
